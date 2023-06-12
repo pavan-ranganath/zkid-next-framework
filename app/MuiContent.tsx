c65d3f06e3b04e84f0ffe1a567f428503b505ed2
pavanr ranganath <pavanr@entradasolutions.com>
2023-06-13 00:50:15 +0530

    ADDED LOGIN AND REGISTER NAVIGATION IN HOME

--- app/MuiContent.tsx
+++ app/MuiContent.tsx
@@ -6,27 +6,25 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 import { DEFAULT_THEME } from "@/app/theme";
+import Link from "next/link";
 
 const MuiContent = () => {
   const { theme } = useTheme();
   const [themeName, setThemeName] = useState(DEFAULT_THEME);
-  useEffect(() => setThemeName(validThemeOrDefault(theme)), [theme]);
 
   return (
     <Stack sx={{ minHeight: "100vh", padding: "64px" }}>
       <Stack sx={{ flexGrow: 1, justifyContent: "center", alignItems: "center" }} component="main">
         <Typography variant="h5">main content with Theme: {`${themeName}`}</Typography>
+        <Link className="underline" href="/login"><Typography variant="h5">Login</Typography></Link>
+        <Link className="underline" href="/register"><Typography variant="h5">Register</Typography></Link>
       </Stack>
       <Stack direction="row" sx={{ justifyContent: "space-between" }} component="footer">
-        <Typography>footer</Typography>
-        <Typography>stuff</Typography>
+        <Typography>Alpha</Typography>
+        <Typography>V1</Typography>
       </Stack>
     </Stack>
   );
 };
 
 export default MuiContent;
-
-function validThemeOrDefault(theme: string | undefined): import("react").SetStateAction<"dark" | "light"> {
-  return 'dark';
-}
