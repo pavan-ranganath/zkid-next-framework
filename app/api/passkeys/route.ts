1995aa5c6be32c9aaa20ecb4797c6595e977fad5
pavanr ranganath <pavanr@entradasolutions.com>
2023-06-27 06:00:05 +0530

    adding passkey working

---
+++ app/api/passkeys/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import mongoose from "mongoose";
+import { Sort } from "mongodb";
+import { dbConnect } from "@/lib/mongodb";
+import { ColumnSort, ColumnFilter } from "@tanstack/table-core";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { credentailsFromTb } from "@/app/dashboard/users/service";
+dbConnect();
+export type filter = {
+  id: string;
+  value: string;
+};
+export async function GET(req: NextRequest, context: any) {
+  const session = await getServerSession(authOptions);
+  const email = session?.user?.email;
+  if (!email) {
+    return NextResponse.json(
+      { error: "Authentication is required" },
+      { status: 401 }
+    );
+  }
+  try {
+    const collection =
+      mongoose.connection.db.collection<credentailsFromTb>("credentials");
+
+    const data = await collection.findOne({ userID: email });
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error processing API request:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,95 @@
 "use client"
 import LogoutButton from "@/components/LogoutButton";
-import { Button } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardHeader, Collapse, Grid, List, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Typography } from "@mui/material";
 import { startRegistration } from "@simplewebauthn/browser";
 import toast from "react-hot-toast";
+import { credentailsFromTb } from "./users/service";
+import { Suspense } from "react";
+import useSWR from 'swr'
 
 export default function Dashboard() {
+
+
   return (
     <>
-      <Button onClick={registerWebauthn}>
-        Add new passkey
-      </Button>
-
+      <h1>Dashboard</h1>
+      <Suspense fallback={<p>Loading data...</p>}>
+        <GetPasskeys />
+      </Suspense>
     </>
   );
 }
 
+async function GetPasskeys() {
+  const { data: userInfo, error, isLoading } = useSWR<credentailsFromTb>('/api/passkeys', fetcher)
+  if (isLoading) return <div>loading...</div>
+  if (!userInfo) {
+    return <div>Error</div>
+  }
+  if (!userInfo.passkeyInfo) {
+    console.log(userInfo)
+    return <div>Error passkeyInfo</div>
+  }
+  // const userInfo: credentailsFromTb = await userInfoResponse.json();
+  return (
+    <>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6}>
+          <Card variant="outlined">
+            <CardHeader title="User info" />
+            <CardContent>
+              <Typography gutterBottom variant="body1" component="div">
+                Email:{userInfo.userInfo?.email}
+              </Typography>
+              <Typography gutterBottom variant="body1" component="div">
+                Firstname: {userInfo.userInfo?.firstName}
+              </Typography>
+              <Typography gutterBottom variant="body1" component="div">
+                Lastname: {userInfo.userInfo?.lastName}
+              </Typography>
+              <Typography gutterBottom variant="body1" component="div">
+                Email verified: {userInfo.userInfo?.emailVerified}
+              </Typography>
+            </CardContent>
+
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card variant="outlined">
+            <CardHeader title="Passkeys" />
+            <CardContent>
+
+              <List
+
+                sx={{ width: '100%' }}
+                component="nav"
+
+              >
+                {userInfo.passkeyInfo?.map((passkey) => (
+                  <ListItemButton>
+                    <ListItemText primary={passkey.friendlyName} />
+                  </ListItemButton>
+                ))
+                }
+              </List>
+            </CardContent>
+            <CardActions>
+              <Button size="small" onClick={registerWebauthn}>Add new passkey</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+
+  )
+}
+export async function fetcher<JSON = any>(
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<JSON> {
+  const res = await fetch(input, init)
+  return res.json()
+}
 async function registerWebauthn() {
   const url = new URL(
     '/api/auth/register/webauthn',
