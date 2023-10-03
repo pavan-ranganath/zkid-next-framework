47b602899f2403db5212c330ffe4b8fc01015a0f
Pavan Ranganath <pavanr@entradasolutions.com>
2023-10-04 01:21:07 +0530

    fixed webpack warnings

--- next.config.js
+++ next.config.js
@@ -1,18 +1,31 @@
+const FilterWarningsPlugin = require("webpack-filter-warnings-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   webpack: (config) => {
-    config.module.rules.push({
-      test: /\.node$/,
-      use: "raw-loader",
+    // Fixes warning from mongodb - https://github.com/Automattic/mongoose/issues/13212#issuecomment-1518012851
+    Object.assign(config.resolve.alias, {
+      "@mongodb-js/zstd": false,
+      "@aws-sdk/credential-providers": false,
+      snappy: false,
+      aws4: false,
+      "mongodb-client-encryption": false,
+      kerberos: false,
+      "supports-color": false,
     });
-    config.experiments = { topLevelAwait: true };
+    config.plugins.push(
+      new FilterWarningsPlugin({
+        exclude: [/the request of a dependency is an expression/],
+      }),
+    );
     return config;
   },
+
   experimental: {
     serverActions: true,
-    esmExternals: "loose",
-    serverComponentsExternalPackages: ["mongoose", "@typegoose/typegoose"],
+    serverComponentsExternalPackages: ["mongoose"],
   },
 };
 
