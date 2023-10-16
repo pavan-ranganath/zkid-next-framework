c5a3e1b993d9313643a88897a8dcfd1756d3b56f
Pavan Ranganath <pavanr@entradasolutions.com>
2023-10-16 17:06:45 +0530

    QR code displayed and button added

--- components/AgeVerificateCertificateDisplay.tsx
+++ components/AgeVerificateCertificateDisplay.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card, CardContent, CardHeader, Grid, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, CardHeader, Grid, Typography } from '@mui/material';
 import QRCode from 'qrcode.react'; // Install qrcode.react
 import { XMLParser } from "fast-xml-parser";
 import { AgeVerificatingCertificate } from '@/lib/interfaces/Certificate.interface';
@@ -29,66 +29,54 @@ export const CertificateDisplay = ({ xmlData }: { xmlData: string }) => {
     if (!certificateInfo.Certificate) {
         return <div>Loading...</div>;
     }
-
+    const flipCard = () => {
+        setShowQR(!showQR);
+    };
     return (
-        // <Card>
-        //     <CardHeader title={certificateInfo.Certificate.name} />
-        //     <CardContent>
-        //         <Grid container spacing={2}>
-        //             <Grid item xs={6}>
-        //                 <Typography variant="h6">Issued By:</Typography>
-        //                 <Typography>{certificateInfo.Certificate.IssuedBy.Organization.name}</Typography>
-        //             </Grid>
-        //             <Grid item xs={6}>
-        //                 <Typography variant="h6">Issued To:</Typography>
-        //                 <Typography>{certificateInfo.Certificate.IssuedTo.Person.name}</Typography>
-        //                 <Image
-        //                     width={100}
-        //                     height={100}
-        //                     src={`data:image/jpeg;base64, ${certificateInfo.Certificate.IssuedTo.Person.Photo._}`}
-        //                     alt="Person's Photo"
-        //                 />
-        //                 <Typography>Claimed Age: {certificateInfo.Certificate.CertificateData?.ZKPROOF?.claimedAge}</Typography>
-        //             </Grid>
-        //         </Grid>
-        //         <QRCode value={"https://google.com"} />
-        //         <Button variant="contained" color="primary">
-        //             Share
-        //         </Button>
-        //         <Button variant="contained" color="primary">
-        //             Download
-        //         </Button>
-        //     </CardContent>
-        // </Card>
-        <Card>
-            <CardContent sx={styles.centeredContent}>
-                <Typography variant="h4">{certificateInfo.Certificate.name} ID</Typography>
-                <Typography variant="h6">Issued By: {certificateInfo.Certificate.IssuedBy.Organization.name}</Typography>
-                <Grid container spacing={2}>
-                    <Grid item xs={4} sx={styles.centeredContent}>
-                        <Image
-                            src={`data:image/jpeg;base64, ${certificateInfo.Certificate.IssuedTo.Person.Photo._}`}
-                            alt="Person's Photo"
-                            width={100}
-                            height={100}
-                        />
+        <Box sx={styles.cardContainer}>
+            <Card sx={{ ...styles.card }}>
+                <CardContent sx={styles.centeredContent}>
+                    <Typography variant="h4">{certificateInfo.Certificate.name} ID</Typography>
+                    <Typography>Issued By: {certificateInfo.Certificate.IssuedBy.Organization.name}</Typography>
+                    <Grid container spacing={2}>
+                        <Grid item xs={4} sx={styles.centeredContent}>
+                            <Image
+                                src={`data:image/jpeg;base64, ${certificateInfo.Certificate.IssuedTo.Person.Photo._}`}
+                                alt="Person's Photo"
+                                width={100}
+                                height={100}
+                            />
+                        </Grid>
+                        <Grid item xs={8} sx={{ ...styles.centeredContent, alignItems: 'start' }}>
+                            <Typography variant="h5">{certificateInfo.Certificate.IssuedTo.Person.name}</Typography>
+                            <Typography variant="h6">Claimed Age: {certificateInfo.Certificate.CertificateData?.ZKPROOF?.claimedAge}</Typography>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={8} sx={{ ...styles.centeredContent, alignItems: 'start' }}>
-                        <Typography variant="h5">{certificateInfo.Certificate.IssuedTo.Person.name}</Typography>
-                        <Typography variant="h6">Claimed Age: {certificateInfo.Certificate.CertificateData?.ZKPROOF?.claimedAge}</Typography>
+                    <Grid container spacing={2}>
+                        <Grid item xs={6} sx={styles.centeredContent}>
+                            <Typography>Issued: {certificateInfo.Certificate.issueDate}</Typography>
+                        </Grid>
+                        <Grid item xs={6} sx={styles.centeredContent}>
+                            <Typography>Expiry: {certificateInfo.Certificate.expiryDate}</Typography>
+                        </Grid>
                     </Grid>
-                </Grid>
-                {showQR && <QRCode value={"https://google.com"} />}
-                <div style={styles.buttonsContainer}>
-                    <Button variant="contained" color="primary">
-                        Share
-                    </Button>
-                    <Button variant="contained" color="primary">
-                        Display QR
-                    </Button>
-                </div>
-            </CardContent>
-        </Card>
+                    {showQR && (<QRCode value={"https://google.com"} />)}
+                    <Box sx={styles.buttonsContainer}>
+                        <Button variant="contained" color="primary" onClick={flipCard}>
+                            {!showQR ? "Display QR" : "Hide QR"}
+                        </Button>
+                        <Button variant="contained" color="primary">
+                            Share
+                        </Button>
+                        <Button variant="contained" color="primary">
+                            Download
+                        </Button>
+                    </Box>
+                </CardContent>
+            </Card>
+
+        </Box>
+
     );
 };
 const styles = {
@@ -98,11 +86,22 @@ const styles = {
         alignItems: 'center',
         justifyContent: 'center',
     },
+    cardContainer: {
+        perspective: '1000px',
+        width: '100%',
+        minHeight: '300px',
+        margin: '0 auto',
+    },
+    card: {
+        width: '100%',
+        height: '100%',
+        transformStyle: 'preserve-3d',
+        transition: 'transform 0.5s',
+    },
     buttonsContainer: {
         marginTop: '16px',
         display: 'flex',
         justifyContent: 'space-between',
-
     },
 };
 export default CertificateDisplay;
