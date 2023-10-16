009fded7afaf137839de780a22462b2bae165d8e
Pavan Ranganath <pavanr@entradasolutions.com>
2023-10-16 16:02:26 +0530

    works without QR code

--- components/AgeVerificateCertificateDisplay.tsx
+++ components/AgeVerificateCertificateDisplay.tsx
@@ -6,6 +6,8 @@ import { AgeVerificatingCertificate } from '@/lib/interfaces/Certificate.interfa
 import Image from 'next/image';
 
 export const CertificateDisplay = ({ xmlData }: { xmlData: string }) => {
+    const [showQR, setShowQR] = useState(false);
+
     const [certificateInfo, setCertificateInfo] = useState<AgeVerificatingCertificate>({} as AgeVerificatingCertificate);
     const parser = new XMLParser({
         attributeNamePrefix: '',
@@ -29,31 +31,78 @@ export const CertificateDisplay = ({ xmlData }: { xmlData: string }) => {
     }
 
     return (
+        // <Card>
+        //     <CardHeader title={certificateInfo.Certificate.name} />
+        //     <CardContent>
+        //         <Grid container spacing={2}>
+        //             <Grid item xs={6}>
+        //                 <Typography variant="h6">Issued By:</Typography>
+        //                 <Typography>{certificateInfo.Certificate.IssuedBy.Organization.name}</Typography>
+        //             </Grid>
+        //             <Grid item xs={6}>
+        //                 <Typography variant="h6">Issued To:</Typography>
+        //                 <Typography>{certificateInfo.Certificate.IssuedTo.Person.name}</Typography>
+        //                 <Image
+        //                     width={100}
+        //                     height={100}
+        //                     src={`data:image/jpeg;base64, ${certificateInfo.Certificate.IssuedTo.Person.Photo._}`}
+        //                     alt="Person's Photo"
+        //                 />
+        //                 <Typography>Claimed Age: {certificateInfo.Certificate.CertificateData?.ZKPROOF?.claimedAge}</Typography>
+        //             </Grid>
+        //         </Grid>
+        //         <QRCode value={"https://google.com"} />
+        //         <Button variant="contained" color="primary">
+        //             Share
+        //         </Button>
+        //         <Button variant="contained" color="primary">
+        //             Download
+        //         </Button>
+        //     </CardContent>
+        // </Card>
         <Card>
-            <CardHeader title={certificateInfo.Certificate.name} />
-            <CardContent>
+            <CardContent sx={styles.centeredContent}>
+                <Typography variant="h4">{certificateInfo.Certificate.name} ID</Typography>
+                <Typography variant="h6">Issued By: {certificateInfo.Certificate.IssuedBy.Organization.name}</Typography>
                 <Grid container spacing={2}>
-                    <Grid item xs={6}>
-                        <Typography variant="h6">Issued By:</Typography>
-                        <Typography>{certificateInfo.Certificate.IssuedBy.Organization.name}</Typography>
+                    <Grid item xs={4} sx={styles.centeredContent}>
+                        <Image
+                            src={`data:image/jpeg;base64, ${certificateInfo.Certificate.IssuedTo.Person.Photo._}`}
+                            alt="Person's Photo"
+                            width={100}
+                            height={100}
+                        />
                     </Grid>
-                    <Grid item xs={6}>
-                        <Typography variant="h6">Issued To:</Typography>
-                        <Typography>{certificateInfo.Certificate.IssuedTo.Person.name}</Typography>
-                        <Image width={100} height={100} src={`data:image/jpeg;base64, ${certificateInfo.Certificate.IssuedTo.Person.Photo._}`} alt="Person's Photo" />
-                        <Typography>Claimed Age: {certificateInfo.Certificate.CertificateData?.ZKPROOF?.claimedAge}</Typography>
+                    <Grid item xs={8} sx={{ ...styles.centeredContent, alignItems: 'start' }}>
+                        <Typography variant="h5">{certificateInfo.Certificate.IssuedTo.Person.name}</Typography>
+                        <Typography variant="h6">Claimed Age: {certificateInfo.Certificate.CertificateData?.ZKPROOF?.claimedAge}</Typography>
                     </Grid>
                 </Grid>
-                <QRCode value={"https://google.com"} />
-                <Button variant="contained" color="primary">
-                    Share
-                </Button>
-                <Button variant="contained" color="primary">
-                    Download
-                </Button>
+                {showQR && <QRCode value={"https://google.com"} />}
+                <div style={styles.buttonsContainer}>
+                    <Button variant="contained" color="primary">
+                        Share
+                    </Button>
+                    <Button variant="contained" color="primary">
+                        Display QR
+                    </Button>
+                </div>
             </CardContent>
         </Card>
     );
 };
+const styles = {
+    centeredContent: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonsContainer: {
+        marginTop: '16px',
+        display: 'flex',
+        justifyContent: 'space-between',
 
+    },
+};
 export default CertificateDisplay;
